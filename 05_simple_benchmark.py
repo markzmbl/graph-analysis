075ef52537c39dba4f98a788f85918ecf548ea44
markzmbl <markus@zimmball.de>
2024-10-18 10:54:23 +0200

    description graph

--- 05_simple_benchmark.py
+++ 05_simple_benchmark.py
@@ -18,5 +18,6 @@ for length_bound in (3, 5, 7):
         log_path = log_directory / f"{graph_path.stem}.csv"
         with graph_path.open("rb") as graph_file:
             graph = pickle.load(graph_file)
-        for cycle in tqdm(iteration_logging(nx.simple_cycles(graph, length_bound=length_bound), log_path)):
+        for cycle in tqdm(iteration_logging(nx.simple_cycles(graph, length_bound=length_bound), log_path),
+                          desc=graph.stem):
             pass
