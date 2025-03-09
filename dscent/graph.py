116a4839920f553f396b94b28b52bb881ab0a7e6
markzmbl <markus@zimmball.de>
2025-03-09 13:28:44 +0100

    09.03

--- dscent/graph.py
+++ dscent/graph.py
@@ -129,8 +129,76 @@ class ExplorationGraph(TransactionGraph):
 
         return nx.subgraph_view(self, filter_edge=activation_filter)
 
+    def _simple_cycles(self, path: BundledPath, cycles: list[BundledPath]) -> tuple[Timestamp, list[BundledPath]]:
+        *_, head = path
+        # v_cur ← v_k
+        current_vertex = head.vertex
+        # t_cur ← min(Tk)
+        current_timestamp = head.begin()
+        # Set closure time explicitly
+        # ct(v_cur) ← t_cur
+        self.closure[current_vertex] = current_timestamp
+        # lastp ← 0
+        closing_time = 0
 
+        # Adjacency View
+        # Out ← {(v_cur, x, t) ∈ E | t_cur < t ≤ ct(x)}
+        # N ← {x ∈ V |(v_cur, x, t) ∈ Out}
+        successor_view = self.activated_edges(current_timestamp)[current_vertex]
 
+        # Determine whether a cycle is present
+        # if s ∈ N then
+        if self.root_vertex in successor_view:
+            # T ← {t | (v_cur, s, t) ∈ Out}
+            cycle_time_sequence = TimeSequence(successor_view[self.root_vertex])
+            # If necessary update current closing_time to the latest cycle closing time
+            # t ← max(T)
+            # if t > lastp then
+            #   lastp ← t
+            closing_time = max(cycle_time_sequence.end(), closing_time)
+            # Build new cycle
+            cycle = BundledPath(path)
+            # Expand(B, v_cur → Ts)
+            cycle.append(MultiTimedVertex(vertex=self.root_vertex, timestamps=cycle_time_sequence))
+            # Memorize new cycle
+            cycles.append(cycle)
+
+        # Iterate other succeeding vertices
+        # for x ∈ N \ {s} do
+        for successor_vertex, successor_timestamps in successor_view.items():
+            if successor_vertex == self.root_vertex:
+                continue
+            # T_x ← {t | (v_cur, x, t) ∈ Out}
+            successor_time_sequence = TimeSequence(successor_timestamps)
+            # T_x′ ← {t ∈ Tx |t < ct(x)}
+            successor_time_sequence.trim_after(upper_limit=self.closure[successor_vertex])
+            # if T_x′ ≠ ∅ then
+            if len(successor_time_sequence) == 0:
+                continue
+
+            # lastx ← AllBundles(s, Expand(B, v_cur -T_x'→ x)
+            next_path = BundledPath(path)
+            next_path.append(MultiTimedVertex(vertex=successor_vertex, timestamps=successor_time_sequence))
+            new_closing_time, cycles = self._simple_cycles(next_path, cycles)
+            # if last_x > lastp then
+            #   lastp ← last_x
+            closing_time = max(new_closing_time, closing_time)
+
+            # t_m ← min({t ∈ Tx | t > last_x})
+            stric_successor_time_sequence = TimeSequence(successor_timestamps)
+            stric_successor_time_sequence.trim_before(new_closing_time)
+            if len(successor_time_sequence) > 0:  # TODO: check
+                # Extend(U(x), (v_cur, t_m))
+                self.closure.add_dependency(
+                    origin=successor_vertex,
+                    dependency=SingleTimedVertex(vertex=current_vertex, timestamp=successor_time_sequence.begin())
+                )
+        if closing_time > current_timestamp:
+            # Unblock(v_cur, lastp)
+            self.closure[current_vertex] = closing_time
+            self.cascade_closure(origin=current_vertex, closing_time=closing_time)
+
+        return closing_time, cycles
 
     def simple_cycles(self, next_candidates_begin_timestamp: Timestamp):
         all_cycles = []
